80409407f78956b64bedab1384ccf1de3ebe8bac
wesleyburlani <wesley@vizir.com.br>
2020-05-17 00:39:40 -0300

    sheel files to get started with game playing

---
+++ build.sh
@@ -0,0 +1,9 @@
+gcc game.c -o bin/game \
+-lallegro \
+-lallegro_image \
+-lallegro_primitives \
+-lallegro_font \
+-lallegro_ttf \
+-lallegro_acodec \
+-lallegro_audio  \
+-pthread -Wall
---
+++ install-deps.sh
@@ -0,0 +1,7 @@
+apt install -y \
+liballegro5-dev \
+liballegro-dialog5-dev \
+liballegro-audio5-dev \
+liballegro-acodec5-dev \
+liballegro-image5-dev \
+liballegro-ttf5-dev
